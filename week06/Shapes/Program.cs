10d2402de905a20d778a4dbcbb3c1cf6256031a7
DannyDzWare <danieldzierzon@gmail.com>
2025-04-04 19:38:07 +0200

    Week 6 Learning Activity Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("green", 2.5),
+            new Rectangle("yellow", 2.5, 5.5),
+            new Circle("red", 2)
+        };
+        
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
