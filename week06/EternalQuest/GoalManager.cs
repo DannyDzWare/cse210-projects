464f1b3d22e9414d4f7d3e45a4de81489fab9f90
DannyDzWare <danieldzierzon@gmail.com>
2025-04-10 19:01:38 +0200

    Exceeding the Requirements

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -13,7 +13,7 @@ public class GoalManager
 
     public void Start()
     {
-
+        
     }
 
     public void DisplayPlayerInfo()
@@ -49,13 +49,20 @@ public class GoalManager
         Console.WriteLine("\t1. Simple Goal");
         Console.WriteLine("\t2. Eternal Goal");
         Console.WriteLine("\t3. Checklist Goal");
+        Console.WriteLine("\t4. Negative Goal");
         Console.Write("Which type of goal would you like to create? ");
         string type = Console.ReadLine();
-        Console.Write("What is the name of your goal? ");
+        string placeholder = "goal";
+        if(type == "4")
+        {
+            placeholder = "bad habbit";
+        }
+        
+        Console.Write($"What is the name of your {placeholder}? ");
         string name = Console.ReadLine();
         Console.Write("What is a short description of it? ");
         string description = Console.ReadLine();
-        Console.Write("What is the amount of points associated with this goal? ");
+        Console.Write($"What is the amount of points associated with this {placeholder}? ");
         string points = Console.ReadLine();
         string target = "0";
         string bonus = "0";
@@ -80,26 +87,38 @@ public class GoalManager
             ChecklistGoal checklistGoal = new ChecklistGoal(name, description,points, Convert.ToInt16(target), Convert.ToInt16(bonus));
             _goals.Add(checklistGoal);
         }
+        else if(type == "4")
+        {
+            NegativeGoal eternalGoal = new NegativeGoal(name, description,points);
+            _goals.Add(eternalGoal);
+        }
         Console.WriteLine("");
     }
    
     public void RecordEvent()
     {
         ListGoalNames();
-        Console.Write("Which goal did you accomplish?");
+        Console.Write("Which goal did you accomplish? ");
         int index = Convert.ToInt16(Console.ReadLine()) - 1;
 
         _goals[index].RecordEvent();
         int points = _goals[index].GetPoints();
         _score += points;
 
-        Console.WriteLine($"Congratulations! You have earned {points} points!");
+        if(points > 0)
+        {
+            Console.WriteLine($"Congratulations! You have earned {points} points!");
+        }
+        else{
+            Console.WriteLine($"So sad! You lost {points} points!");
+            Console.WriteLine($"You can do better!");
+        }
         Console.WriteLine($"You now have {_score} points!\n");
     }
     
     public void SaveGoals()
     {
-        Console.Write("Wat is the filename for the goal file?");
+        Console.Write("Wat is the filename for the goal file? ");
         string filename = Console.ReadLine();
 
         List<string> fileContent = new List<string>();
@@ -109,11 +128,12 @@ public class GoalManager
             fileContent.Add(goal.GetStringRepresentation());
         }
         File.WriteAllLines(filename, fileContent);
+        Console.WriteLine("");
     }
     
     public void LoadGoals()
     {
-        Console.Write("Wat is the filename for the goal file?");
+        Console.Write("Wat is the filename for the goal file? ");
         string filename = Console.ReadLine();
 
         _goals = new List<Goal>();
@@ -123,7 +143,7 @@ public class GoalManager
         _score = Convert.ToInt32(fileContent[0]);
         for(int i = 1; i < fileContent.Count; i++)
         {
-            string[] values = fileContent[i].Split(';');
+            string[] values = fileContent[i].Split('|');
             if(values[0].ToLower() == "simplegoal")
             {
                 SimpleGoal simpleGoal = new SimpleGoal(values[1], values[2], values[3], values[4]);
@@ -139,6 +159,12 @@ public class GoalManager
                 ChecklistGoal checklistGoal = new ChecklistGoal(values[1], values[2], values[3], Convert.ToInt32(values[4]), Convert.ToInt32(values[5]), Convert.ToInt32(values[6]));
                 _goals.Add(checklistGoal);
             }
+            else if(values[0].ToLower() == "negativegoal")
+            {
+                NegativeGoal eternalGoal = new NegativeGoal(values[1], values[2], values[3]);
+                _goals.Add(eternalGoal);
+            }
         }
+        Console.WriteLine("");
     }
 }
--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,6 +1,14 @@
 using System;
 using System.Diagnostics;
 
+
+/*
+    As Exceeding requirement I added negative goals. They are fully integrated.
+    That means you can save and load them too.
+    You can give the points for them in negative or positive form, but it will always remove them from score.
+    Also in the createGoal section the word goal will be replaced with bad habbits, if negative Goal is chosen.
+    When I record a negative goal the output phrase changed and a small sentence of motivation will be shown additionally.
+*/
 class Program
 {
     static void Main(string[] args)
@@ -37,6 +45,6 @@ class Program
         System.Console.WriteLine("\t4. Load Goals");
         System.Console.WriteLine("\t5. Record Event");
         System.Console.WriteLine("\t6. Quit");
-        System.Console.Write("Select a choice from the menu:");
+        System.Console.Write("Select a choice from the menu: ");
     }
 }
